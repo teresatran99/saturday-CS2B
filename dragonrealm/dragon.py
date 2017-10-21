1342d468944d07c25ce4c6b76f24173ff54404a4
teresatran99 <32421817+teresatran99@users.noreply.github.com>
2017-10-21 09:50:07 -0700

    Added more obstacles

--- dragonrealm/dragon.py
+++ dragonrealm/dragon.py
@@ -6,6 +6,9 @@
 import random
 import time
 
+# global variable
+score = 0
+
 # Show Introduction
 def show_intro():
     print('''You are in a land full of dragons. In front of you,
@@ -14,7 +17,6 @@ and will share his treasure with you. The other dragon
 is greedy and hungry, and will eat you on site.''')
     print()
 
-
 def choose_cave():
     cave = ""
     while cave != "1" and cave != "2":
@@ -22,8 +24,86 @@ def choose_cave():
         cave = input()
     return cave
 
-show_intro()
-choose_cave()
+
+def check_cave(cave_chosen):
+    global score
+    print("You approach the cave slowly...")
+    time.sleep(2)
+    print("Smells like dragon farts...")
+    time.sleep(2)
+    print("A big ol'DRAGON jumps out in front of you and opens its jaws and...")
+    print()
+    time.sleep(2)
+
+    friendlyDragon = random.randint(1, 2)
+
+    if cave_chosen == str(friendlyDragon):
+        print("Gives you his treasure!")
+        score += 1
+    else:
+        print("He farts on you.")
+        dodge_fart = random.randint(1, 2)
+        print("You try to dodge the fart. Which way? (l or r))")
+        dodge = input()
+        if dodge == "l" and dodge_fart == "r" :
+            print("You dodged the fart!")
+        else:
+            print("You got hit by the fart!")
+
+
+            
+        print('''Now you must walk across a bridge to reach the second
+    checkpoint. You will meet the headmasters of the dragonrealm. One will
+    give you a few riddles and the other will test your luck.''')
+        time.sleep(3)
+        print("You walk across the bridge and realize how unstable it's been constructed-")
+        time.sleep(2)
+        print('''Run! The board under your feet starts to break and
+              it won't be able to hold you up much longer.''')
+        time.sleep(2)
+        print('''You reach the end of the bridge and see two old men dressed in bizarre
+              wizard-like outfits''')
+        print("The man on the right introduces himself.")
+        time.sleep(2)
+        print('''"Hello! I am a headmaster of this realm. If you want to get pass me,
+                you have to answer at least one riddle correctly..."''')
+        print("Over fire and over stone,")
+        print("Over water and over bone,")
+        print("Shining out like jewels of light,")
+        print("On a sheet of purest night.")
+        print("What are we?")
+    correct_answer = random.randint(1, 2)
+    correct_answer = input()
+    if correct_answer == "stars" and correct_answer == "stars" :
+        print("congrats! you shall pass")
+    else:
+        print("Incorrect! Here's another riddle:")
+        print("I can fly yet have no wings,")
+        print("I beat down mountains, I conquer kings,")
+        print("At once three different things am I,")
+        print("As a continuous whole, I cannot die.")
+        print("Do you know who I am?")
+        print("HINT: it's not a physical object")
+    if correct_answer == "time" and correct_answer == "time" :
+        print("congrats! you shall pass and receive the treasure,")
+    else:
+        print("Aw, wrong answer. You shall pass without receiving the treasure")
+  
+def play():
+    stillPlaying = True
+    while stillPlaying:
+        show_intro()
+        cave = choose_cave()
+        check_cave(cave)
+        print("Would you like to play again? (y to continue, q to quit)")
+        choice = input()
+        if choice == "q":
+            stillPlaying = False
+    print("Your score: " + str(score))
+    print("Goodbye! Thanks for playin', naamean?")
+
+# Play the game!
+play()
 
 
 
